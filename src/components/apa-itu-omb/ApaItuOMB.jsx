f30633713b8691d55b849a224787f41f716df290
Abigail <abigailvaniapra@gmail.com>
2025-02-02 15:05:02 +0700

    Fixing default css

---
+++ src/components/apa-itu-omb/ApaItuOMB.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ApaItuOMB(){
+    return (
+        <>
+            <section className="apaItuOMB">
+                <div className="apaItuOMBHeader">
+                    <h1>Apa itu OMB?</h1>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default ApaItuOMB
