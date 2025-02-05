84d7afeacfac26143fa4630c42968ea85d9b07f4
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-05 22:05:27 +0700

    .

--- src/components/youtube/Youtube.jsx
+++ src/components/youtube/Youtube.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import './Youtube.scss';
+
 
 function Youtube() {
     return (
         <>
-            <section className="Youtube">
-                <div className="YoutubeHeader">
-                    <h1>Kura-kura Matahari merangkai memori</h1>
-                    <h1>Selamat datang para pejuang Kura-kura Matahari!</h1>
-                </div>
-                <div className="YoutubeThumbnail">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/uzkX9F6AkKU?si=eA2lIn2KHLYr8PgF" 
-                            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
-                            clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <section className="YoutubeContainer">
+                <div className="Youtube">
+                    <div className="YoutubeHeader">
+                        <h1>Kura-kura Matahari merangkai memori</h1>
+                        <h1>Selamat datang para pejuang Kura-kura Matahari!</h1>
+                    </div>
+                    <div className="YoutubeThumbnail">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/uzkX9F6AkKU?si=eA2lIn2KHLYr8PgF" 
+                                title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; 
+                                clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    </div>
                 </div>
             </section>
         </>
-    )
+    );
 }
 
-export default Youtube
+export default Youtube;
