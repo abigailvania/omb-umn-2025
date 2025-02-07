640d8fb6129cdab09b1cc691f45da0b706a40ae7
Abigail <abigailvaniapra@gmail.com>
2025-02-07 20:50:49 +0700

    Adding link const for division

--- src/components/oprec/ContentDivisi.jsx
+++ src/components/oprec/ContentDivisi.jsx
@@ -31,6 +31,7 @@ const ContentDivisi = [
         arti: "Ambisi yang Tinggi",
         deskripsi: "Demung merupakan alat musik tradisional dari Jawa Tengah yang menjadi pembuka dan penentu lagu dalam Gamelan. Tanpa adanya pembuka yang terarah, harmoni dalam sebuah lagu tidak akan tercipta. Sama seperti Demung, Arutala berperan sebagai penentu konsep utama dan pemantik semangat dalam membuka perjalanan penuh makna bagi para Peserta OMB UMN 2025. Hal ini selaras dengan makna Arutala, yakni ambisi yang tinggi, sehingga seluruh rangkaian kegiatan akan dipandu berdasarkan konsep yang telah dirancang secara bersama-sama.",
         tugas: "Arutala bertugas merancang dan mematangkan konsep dari rangkaian kegiatan OMB UMN 2025.",
+        link: "",
     },
     {
         logoImg: Dokumentasi,
@@ -40,6 +41,9 @@ const ContentDivisi = [
         arti: "Cahaya Pagi",
         deskripsi: "Atowo adalah alat musik tradisional Papua yang terbuat dari kayu, dimainkan dengan cara dipukul untuk menghasilkan ritme yang melambangkan persatuan dan kerja sama dalam upacara adat. Seperti Arunika, yang menangkap dan menyatukan setiap momen, menjaga alur cerita kegiatan dengan harmoni yang selaras, serta memastikan setiap kenangan tersimpan dengan sempurna.",
         tugas: "Arunika bertugas untuk mengabadikan momen yang ada dalam seluruh rangkaian kegiatan OMB UMN 2025.",
+        link: "",
+        link
+            : "",
     },
     {
         logoImg: Perlengkapan,
@@ -49,6 +53,8 @@ const ContentDivisi = [
         arti: "Pemeliharaan dan Penyediaan",
         deskripsi: "Bende merupakan alat musik tradisional dari Lampung yang berperan sebagai pengumpul dan penggerak masyarakat melalui bunyinya yang nyaring. Sama seperti Bende yang menjadi simbol koordinasi dan pemersatu, Bharana berperan dalam pemeliharaan dan penyediaan kebutuhan logistik, menggerakkan semangat, dan memperkuat perjalanan penuh kebersamaan bagi seluruh peserta OMB UMN 2025.",
         tugas: "Bharana bertugas untuk mempersiapkan segala kebutuhan perlengkapan dalam seluruh rangkaian kegiatan OMB UMN 2025.",
+        link: "",
+
     },
     {
         logoImg: Konsumsi,
@@ -58,6 +64,8 @@ const ContentDivisi = [
         arti: "Dermawan",
         deskripsi: "Dambus, alat musik tradisional Bangka Belitung, melambangkan harmoni dan keberagaman. Harmoni tercipta dari perpaduan senar yang bekerja bersama menghasilkan melodi indah, sementara keberagaman tercermin dalam nada-nada unik yang saling melengkapi. Filosofi ini mengajarkan bahwa keseimbangan dan perbedaan dapat bersatu menciptakan keindahan. Sama halnya dengan divisi Kayana yang akan menyediakan cita rasa beragam yang akan menciptakan harmonisasi indah bagi panitia OMB UMN 2025.",
         tugas: "Kayana bertugas menyediakan makanan dan minuman selama rangkaian OMB UMN 2025.",
+        link: "",
+
     },
     {
         logoImg: Desain,
@@ -67,6 +75,8 @@ const ContentDivisi = [
         arti: "Bintang yang Cantik",
         deskripsi: "Serune Kalee merupakan alat musik tradisional asal aceh yang terbuat dari kombinasi bahan kayu, kuningan, dan tembaga. Hal ini melambangkan Rekha yang memiliki semangat dan ketangguhan dalam merancang segala keperluan desain OMB UMN 2025. Bentuk pangkalnya yang unik  di bagian peniupnya yang ramping dan semakin melebar seperti corong di bagian ujungnya, menggambarkan Rekha yang mampu menghasilkan desain yang unik serta inovatif selama rangkaian OMB UMN 2025",
         tugas: "Rekha bertugas untuk menyusun seluruh keperluan desain OMB UMN 2025 agar memiliki nilai estetika yang tinggi, unik serta mampu menjadi sarana penyampaian informasi yang efektif sesuai dengan nilai-nilai OMB UMN 2025.",
+        link: "",
+
     },
     {
         logoImg: Pr,
@@ -76,6 +86,8 @@ const ContentDivisi = [
         arti: "Jelas dan Pandai Bicara",
         deskripsi: "Kompang merupakan alat musik dari Jawa Timur yang terbuat dari kulit dan kayu, dimainkan secara bersamaan untuk menghasilkan suara yang ritmis dan harmonis, mencerminkan tanggung jawab Wistara dalam menjaga keterpaduan, ketepatan waktu, dan komunikasi yang harmonis, serta cekatan dalam menyampaikan dan menjaga informasi demi menjaga reputasi dan hubungan yang baik dengan berbagai pihak.",
         tugas: "Wistara bertugas untuk membuat dan menyampaikan informasi dengan jelas, rinci, dan akurat, serta menjaga reputasi baik OMB UMN 2025.",
+        link: "",
+
     },
     {
         logoImg: Website,
@@ -85,6 +97,8 @@ const ContentDivisi = [
         arti: "Kumpulan, Disatukan, Dipersatukan",
         deskripsi: "Sampe merupakan alat musik yang berasal dari Kalimantan Timur dan dimainkan dengan cara dipetik. Sampe dihias dengan menggunakan berbagai ukiran indah yang memiliki makna tersendiri. Sampe biasanya digunakan untuk menyatakan perasaan agar dapat diterima,sama seperti SAMHITA yang bertugas untuk menyampaikan segala informasi dengan tepat. Sampe juga terbuat dari kayu pilihan berkualitas baik, seperti SAMHITA yang menyediakan wadah informasi berdasarkan konten pilihan dan terjamin kebenarannya.",
         tugas: "Samhita bertugas dalam merancang dan mengembangkan website resmi OMB UMN 2025 untuk mempersatukan peserta OMB UMN 2025 dengan menyediakan wadah informasi.",
+        link: "",
+
     },
     {
         logoImg: Pic,
@@ -94,6 +108,7 @@ const ContentDivisi = [
         arti: "Pengetahuan",
         deskripsi: "Panting adalah alat musik tradisional dari Kalimantan Selatan yang memiliki syair berisi petuah dan nasihat, menjadikannya sarana penyebaran pengetahuan. Sama seperti Gyana, musik panting dimainkan secara ansambel yang setiap individunya memiliki peran untuk saling melengkapi, menciptakan harmoni yang indah dalam permainan musik.",
         tugas: "Gyana bertugas untuk menuntun dan membekali peserta OMB UMN 2025 agar dapat dengan mudah beradaptasi dengan lingkungan kampus yang baru.",
+        link: "",
     },
     {
         logoImg: Keamanan,
@@ -103,6 +118,8 @@ const ContentDivisi = [
         arti: "Pemberani, Percaya Diri dan Berhati Mulia",
         deskripsi: "Keso-keso adalah alat musik tradisional yang berasal dari Sulawesi Selatan. Keso-keso memerlukan alat untuk menggesek dawai, sama seperti Abhiseva yang membutuhkan satu sama lain sebagai fondasi yang kokoh dalam menciptakan lingkungan yang aman dan nyaman selama OMB UMN 2025.",
         tugas: "Abhiseva bertugas dalam menjaga keamanan dan ketertiban selama rangkaian kegiatan OMB UMN 2025.",
+        link: "",
+
     },
 ]
 
