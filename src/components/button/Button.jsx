ec757a35759d9907532a87a0b9a1bf0a33c41c05
Abigail <abigailvaniapra@gmail.com>
2025-02-07 19:54:51 +0700

    Adding onclick to button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./button.scss";
 
-const GlassButton = ({ text }) => {
-    return <button className="glass-button">{text}</button>;
+const GlassButton = ({ text, onClick }) => {
+    return (
+        <button className="glass-button" onClick={onClick}>
+            {text}
+        </button>
+    );
 };
 
 export default GlassButton;
-
