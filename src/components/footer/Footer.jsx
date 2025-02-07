b7b3e97095b7e39b60df1102e0176eb9e760e0fa
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-07 17:56:40 +0700

    .

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,43 +11,38 @@ import ombaksantuytop from "../../images/ombak/Ombaksantuy_Top.png";
 
 function Footer() {
     return (
-        <div className="footer-container">
-            {/* Ombak */}
-            <div className="ombak">
-                <img src={ombaksantuytop} alt="ombak" />
-            </div>
-
-            {/* Footer Section */}
-            <div className="footerSection">
-                <div className="content">
-                    <div className="flex flex-row justify-between items-center">
-                        <div className="flex-1 text-right">
-                            <p className="text-xl font-bold">Alamat</p>
-                            <p>Universitas Multimedia Nusantara</p> 
-                            <p>Jl. Scientia Boulevard, Gading Serpong,</p>
-                            <p>Tangerang Banten 15811, Indonesia.</p>
-                        </div>
-                        <div className="flex-1 flex flex-col items-center">
-                            <img src={logo} alt="Logo OMB UMN 2025" className="h-[270px]" />
-                            <img src={tagline} alt="Tagline OMB UMN 2025" className="h-[100px] -mt-[20px]" />
-                        </div>
-                        <div className="flex-1">
-                            <p className="text-xl font-bold">Pranala</p>
-                            <p>YouTube: @ombumn</p>
-                            <p>Spotify: Swara Bestari</p>
-                            <p>Website: omb.umn.ac.id</p>
-                        </div>
+        <div className="footerSection">
+            <div className="content">
+                <div className="ombak mb-[100px]">
+                    <img src={ombaksantuytop} alt="ombak" />
+                </div>
+                <div className="flex flex-row justify-between items-center">
+                    <div className="flex-1 text-right">
+                        <p className="text-xl font-bold">Alamat</p>
+                        <p>Universitas Multimedia Nusantara</p> 
+                        <p>Jl. Scientia Boulevard, Gading Serpong,</p>
+                        <p>Tangerang Banten 15811, Indonesia.</p>
                     </div>
-                    <div className="flex flex-col justify-between items-center mt-4">
-                        <div className="flex flex-row">
-                            <a href="#"> <img src={email} alt="email" className="w-[25px] mx-[10px]"/></a>
-                            <a href="https://www.instagram.com/ombumn"> <img src={insta} alt="instagram" className="w-[25px] mx-[10px]" /></a>
-                            <a href="https://linevoom.line.me/user/_dQto-n6wJSbyUlsQVlEfNHoStsNJwzSLtfVj2Xo"> <img src={line} alt="line" className="w-[25px] mx-[10px]" /></a>
-                            <a href="https://www.facebook.com/ombumn/?_rdc=2&_rdr"> <img src={facebook} alt="facebook" className="w-[25px] mx-[10px]" /></a>
-                        </div>
-                        <div className="my-6">
-                            <p>© OMB UMN 2025</p>
-                        </div>
+                    <div className="flex-1 flex flex-col items-center">
+                        <img src={logo} alt="Logo OMB UMN 2025" className="h-[270px]" />
+                        <img src={tagline} alt="Tagline OMB UMN 2025" className="h-[100px] -mt-[20px]" />
+                    </div>
+                    <div className="flex-1">
+                        <p className="text-xl font-bold">Pranala</p>
+                        <p>YouTube: @ombumn</p>
+                        <p>Spotify: Swara Bestari</p>
+                        <p>Website: omb.umn.ac.id</p>
+                    </div>
+                </div>
+                <div className="flex flex-col justify-between items-center mt-4">
+                    <div className="flex flex-row">
+                        <a href="#"> <img src={email} alt="email" className="w-[25px] mx-[10px]"/></a>
+                        <a href="https://www.instagram.com/ombumn"> <img src={insta} alt="instagram" className="w-[25px] mx-[10px]" /></a>
+                        <a href="https://linevoom.line.me/user/_dQto-n6wJSbyUlsQVlEfNHoStsNJwzSLtfVj2Xo"> <img src={line} alt="line" className="w-[25px] mx-[10px]" /></a>
+                        <a href="https://www.facebook.com/ombumn/?_rdc=2&_rdr"> <img src={facebook} alt="facebook" className="w-[25px] mx-[10px]" /></a>
+                    </div>
+                    <div className="my-6">
+                        <p>© OMB UMN 2025</p>
                     </div>
                 </div>
             </div>
