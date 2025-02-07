d4e086174b346821d30e8b543440a4719b996446
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-07 12:51:39 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ApaItuOMB from './components/apa-itu-omb/ApaItuOMB'
 import Youtube from './components/youtube/Youtube'
 import Oprec from './components/oprec/Oprec'
 import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <Header />
+      <Navbar />
       <ApaItuOMB />
       <Youtube />
       <Oprec />
