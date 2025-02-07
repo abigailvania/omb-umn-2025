a82ec38b011d6e222884ddc24190f30c54d8e3a7
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-08 01:47:27 +0700

    a

--- src/components/oprec/Oprec.jsx
+++ src/components/oprec/Oprec.jsx
@@ -58,18 +58,18 @@ function Oprec() {
                         <img src={supergrafis2} alt="supergrafis" className="z-0" />
                     </div>
                 </div>
-                <div className="flex flex-col p-2 z-10">
-                    <div className="flex flex-1 justify-center z-10">
+                <div className="logo-divisi flex flex-col p-2 z-10">
+                    <div className="baris1 flex flex-1 justify-center z-10">
                         {ContentDivisi.slice(0, 4).map((divisi, index) => (
                             <DivisiCard key={index} divisi={divisi} onClick={setSelectedDivisi} />
                         ))}
                     </div>
-                    <div className="flex flex-1 justify-center z-10">
+                    <div className="baris2 flex flex-1 justify-center z-10">
                         {ContentDivisi.slice(4, 7).map((divisi, index) => (
                             <DivisiCard key={index} divisi={divisi} onClick={setSelectedDivisi} />
                         ))}
                     </div>
-                    <div className="flex flex-1 justify-center z-10">
+                    <div className="baris3 flex flex-1 justify-center z-10">
                         {ContentDivisi.slice(7, 9).map((divisi, index) => (
                             <DivisiCard key={index} divisi={divisi} onClick={setSelectedDivisi} />
                         ))}
