36ea7818121dd8b720d42b24292239fb88ede134
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-07 18:31:21 +0700

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,33 +9,35 @@ import GlassButton from "../button/Button.jsx";
 
 function Header() {
     return (
-        <>
-            <section className="headerSection">
+        <section className="headerSection">
+            <div className="headerContent">
                 <div className="flex flex-col justify-center align-center md:pt-18">
                     <div className="flex justify-center">
                         <img src={logo} alt="" className="h-[270px]" />
                     </div>
-                    <div className="flex justify-center -mt-[60px] align-center 
-                                    md:mt-0">
+                    <div className="flex justify-center -mt-[60px] align-center md:mt-0">
                         <img src={tagline} alt="Tagline OMB UMN 2025" className="h-[95px]" />
                     </div>
                 </div>
                 <div className="flex flex-col justify-center items-center text-center text-white mt-[20px]">
                     <p className="text-xl mb-10">
-                        Pendaftaran Kepanitiaan OMB UMN 2025 Telah Dibuka!</p>
+                        Pendaftaran Kepanitiaan OMB UMN 2025 Telah Dibuka!
+                    </p>
                     <GlassButton
                         text="Daftar Sekarang!"
                         onClick={() => document.getElementById("oprec-section").scrollIntoView({ behavior: "smooth" })}
                         className="text-white max-w-[180px] mt-[20px]"
                     />
                 </div>
-                <div className="ombak">
-                    <img src={ombakterangdown} alt="ombak"/>
-                </div>
+            </div>
 
-            </section>        
-        </>
-    )
+            <div className="ombak">
+                <img src={ombakterangdown} alt="ombak"/>
+            </div>
+        </section>
+    );
 }
 
-export default Header
+export default Header;
+
+
