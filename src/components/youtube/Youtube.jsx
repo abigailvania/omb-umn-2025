d1ad6a17668bb0aab6c9e9d488c49ac9210c586f
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-09 00:20:53 +0700

    aaaaaaaaaaa

--- src/components/youtube/Youtube.jsx
+++ src/components/youtube/Youtube.jsx
@@ -19,7 +19,7 @@ function Youtube() {
                         <h1>Selamat datang para pejuang Kura-kura Matahari!</h1>
                     </div>
                     <div className="YoutubeThumbnail">
-                        <iframe width="600" height="337" src="https://www.youtube.com/embed/uzkX9F6AkKU?si=eA2lIn2KHLYr8PgF" 
+                        <iframe width="700" height="437" src="https://www.youtube.com/embed/uzkX9F6AkKU?si=eA2lIn2KHLYr8PgF" 
                                 title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; 
                                 clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                                 referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
