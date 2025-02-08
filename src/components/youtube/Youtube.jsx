96da32d459ea4d7753cdad6fd9e3a5d093ef9cc1
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-09 01:03:13 +0700

    a

--- src/components/youtube/Youtube.jsx
+++ src/components/youtube/Youtube.jsx
@@ -25,7 +25,7 @@ function Youtube() {
                                 referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                     </div>
                 </div>
-                <div className="ombak mt-[-250px] mb-[-300px]">
+                <div className="ombak mt-[-250px] ">
                     <img src={ombak} alt="ombak" />
                 </div>
             </section>
