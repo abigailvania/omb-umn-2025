8b6e0fe08a01814f110ead24136569f0f72df504
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-08 21:40:39 +0700

    a

--- src/components/oprec/Oprec.jsx
+++ src/components/oprec/Oprec.jsx
@@ -86,7 +86,7 @@ function Oprec() {
                             animate={{ opacity: 1, scale: 1 }}
                             exit={{ opacity: 0, scale: 0.9 }}
                             transition={{ duration: 0.3, ease: "easeOut" }}
-                            className="relative bg-gray-100 p-6 max-w-[700px] w-full max-h-[65vh] overflow-y-auto rounded-xl shadow-lg flex flex-col"
+                            className="relative bg-gray-100 p-6 max-w-[900px] w-full max-h-[65vh] overflow-y-auto rounded-xl shadow-lg flex flex-col"
                         >
                             <button
                                 className="absolute z-10 top-3 right-3 text-gray-600 hover:text-gray-900 text-2xl px-2 rounded-full bg-gray-300 cursor-pointer flex items-center justify-center w-10 h-10 shadow-md"
