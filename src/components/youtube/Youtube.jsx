060ee26151e897d67160e0d265bd946cd73708a1
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-08 23:35:01 +0700

    .

--- src/components/youtube/Youtube.jsx
+++ src/components/youtube/Youtube.jsx
@@ -19,9 +19,6 @@ function Youtube() {
                         <h1>Selamat datang para pejuang Kura-kura Matahari!</h1>
                     </div>
                     <div className="YoutubeThumbnail">
-                        {/* <div className="supergrafis">
-                            <img src={supergrafis} alt="supergrafis" />
-                        </div> */}
                         <iframe width="600" height="337" src="https://www.youtube.com/embed/uzkX9F6AkKU?si=eA2lIn2KHLYr8PgF" 
                                 title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; 
                                 clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
