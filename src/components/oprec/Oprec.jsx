51c264342a9d1f6bc2ca4f5d7bdb0f6cf8400f2f
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-08 21:33:39 +0700

    a

--- src/components/oprec/Oprec.jsx
+++ src/components/oprec/Oprec.jsx
@@ -35,7 +35,7 @@ function Oprec() {
     return (
         <>
             <section className="oprec">
-                <div id="oprec-section" className="OprecText mt-[20px]">
+                <div id="oprec-section" className="OprecText">
                     {/* <div className="supergrafis1">
                         <img src={supergrafis1} alt="supergrafis" />
                     </div> */}
