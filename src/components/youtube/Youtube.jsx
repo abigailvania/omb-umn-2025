01f40aaae762d890bf7456b5f4e14431bfec396e
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-09 10:31:44 +0700

    youtube

--- src/components/youtube/Youtube.jsx
+++ src/components/youtube/Youtube.jsx
@@ -14,7 +14,7 @@ function Youtube() {
                 </div>
                 
                 <div className="Youtube">
-                    <div className="YoutubeHeader mt-[50px]">
+                    <div className="YoutubeHeader mt-[80px]">
                         <h1>Kura-kura Matahari merangkai memori, <br />Selamat datang para pejuang Kura-kura Matahari!</h1>
                     </div>
                     <div className="YoutubeThumbnail ">
