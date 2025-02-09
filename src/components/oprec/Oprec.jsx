6f2d3feb184dbbff6e28bbfb5974b1d4c9757e86
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-09 10:04:35 +0700

    revisi

--- src/components/oprec/Oprec.jsx
+++ src/components/oprec/Oprec.jsx
@@ -32,7 +32,7 @@ function Oprec() {
 
     return (
         <>
-            <section className="oprec">
+            <section className="oprec pt-[-100px]">
                 <div className="supergrafis pt-[-200px] opacity-40">
                     <img src={supergrafis} alt="supergrafis" />
                 </div>
