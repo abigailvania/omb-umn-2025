a4b494c318e12aa20eb2f2260ba520c576a8bfc5
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-09 20:23:19 +0700

    perbaiki klik divisi

--- src/components/oprec/Oprec.jsx
+++ src/components/oprec/Oprec.jsx
@@ -2,23 +2,16 @@ import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import "./oprec.scss";
 import Countdown from "./countdown/Countdown.jsx";
-
 import ContentDivisi from "./ContentDivisi";
-
 import GlassButton from "../button/Button.jsx";
 import ImageSlider from "../image-slider/image-slider.jsx";
-
 import ombak from "../../images/ombak/OMBAK4.png";
 import supergrafis from "../../images/supergrafis/SUP3.png";
 
 const DivisiCard = ({ divisi, onClick }) => {
     return (
         <div className="flex flex-col items-center cursor-pointer" onClick={() => onClick(divisi)}>
-            <img
-                src={divisi.logoImg}
-                alt={divisi.name}
-                className=""
-            />
+            <img src={divisi.logoImg} alt={divisi.name} className="" />
         </div>
     );
 };
@@ -29,6 +22,12 @@ function Oprec() {
     const images = selectedDivisi
         ? [selectedDivisi.photo1, selectedDivisi.photo2, selectedDivisi.photo3].filter(Boolean)
         : [];
+
+    const closeModal = (e) => {
+        if (e.target.id === "modal-container") {
+            setSelectedDivisi(null);
+        }
+    };
 
     return (
         <>
@@ -43,11 +42,7 @@ function Oprec() {
                     </div>
                     <div className="OprecDetail">
                         <h2>Siapkah kamu menjadi bagian dari OMB UMN 2025?</h2>
-                        <p>
-                            Pendaftaran terbuka bagi mahasiswa/i aktif seluruh program studi dari angkatan 2022, 2023 dan 2024
-                            yang telah lulus mengikuti rangkaian kegiatan OMB UMN. Form pendaftaran terbuka dari 10 Februari
-                            2025 pukul 08.00 WIB sampai 15 Februari 2025 pukul 23.59 WIB.
-                        </p>
+                        <p>Pendaftaran terbuka dari 10 Februari 2025 pukul 08.00 WIB sampai 15 Februari 2025 pukul 23.59 WIB.</p>
                     </div>
                     <div className="countdown-close">
                         <Countdown targetDate="2025-02-15T23:59:00" />
@@ -55,82 +50,72 @@ function Oprec() {
                 </div>
                 <div id="oprec-section" className="OprecDivisi">
                     <div className="DivisiHeader">
-
                         <h1>DIVISI</h1>
-                        <h6>
-                            Yuk, jelajahi divisi-divisi yang ada di OMB UMN 2025 dan jadi bagian untuk mengukir sejarah baru!
-                            <br />
-                            Klik divisi untuk melihat informasi lebih lanjut.
-                        </h6>
+                        <h6>Klik divisi untuk melihat informasi lebih lanjut.</h6>
                     </div>
                     <div className="logo-divisi z-10 mb-[100px]">
                         {ContentDivisi.slice(0, 9).map((divisi, index) => (
                             <DivisiCard key={index} divisi={divisi} onClick={setSelectedDivisi} />
                         ))}
-
                     </div>
                 </div>
                 <div className="ombak">
                     <img src={ombak} alt="ombak" />
                 </div>
             </section>
-            {selectedDivisi && (
-                <AnimatePresence>
-                    <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50 text-blue-950">
+            
+            <AnimatePresence>
+                {selectedDivisi && (
+                    <div id="modal-container" onClick={closeModal} className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
                         <motion.div
                             initial={{ opacity: 0, scale: 0.9 }}
                             animate={{ opacity: 1, scale: 1 }}
                             exit={{ opacity: 0, scale: 0.9 }}
                             transition={{ duration: 0.3, ease: "easeOut" }}
-                            className="relative bg-gray-100 p-6 max-w-[600px] w-full max-h-[70vh] overflow-y-auto rounded-xl shadow-lg flex flex-col z-100 mt-[7%]"
+                            className="relative bg-gray-100 p-6 max-w-[600px] w-full max-h-[70vh] overflow-y-auto rounded-xl shadow-lg flex flex-col"
                         >
                             <button
-                                className="absolute z-10 top-3 right-3 text-gray-600 hover:text-gray-900 text-2xl px-2 rounded-full bg-gray-300 cursor-pointer flex items-center justify-center w-10 h-10 shadow-md"
+                                className="absolute top-3 right-3 text-gray-600 hover:text-gray-900 text-2xl px-2 rounded-full bg-gray-300 cursor-pointer"
                                 onClick={() => setSelectedDivisi(null)}
                             >
                                 &times;
                             </button>
 
-                            <div className="">
-                                <div className="flex flex-col md:flex-row items-center text-center">
-                                    <div className="flex-row mx-4 w-full mb-4">
-                                        <h2 className="text-2xl font-bold">{selectedDivisi.name}</h2>
-                                        <h3 className="text-lg font-semibold">{selectedDivisi.nama}</h3>
-
-                                        <div className="flex justify-center my-4">
-                                            <img
-                                                src={selectedDivisi.logoImg}
-                                                alt={selectedDivisi.name}
-                                                className="w-[120px] object-contain rounded-full shadow-md"
-                                            />
-                                        </div>
-
-                                        <div className="bg-gray-200 p-2 rounded-lg border border-blue-950 w-full">
-                                            <p className="text-sm">Logo: {selectedDivisi.logo}</p>
-                                            <p className="text-sm">Arti: {selectedDivisi.arti}</p>
-                                        </div>
-                                    </div>
-
-                                    <div className="">
-                                        <div className="foto-divisi">
-                                            <ImageSlider images={images} interval={3000} />
-                                        </div>
-                                        <div className="bg-gray-200 p-4 mt-4 rounded-lg border border-blue-950 w-full mb-[20px]">
-                                            <p className="text-sm text-justify">{selectedDivisi.deskripsi}</p>
-                                            <p className="text-sm mt-2 text-justify">{selectedDivisi.tugas}</p>
-                                        </div>
-                                        <GlassButton
-                                            text="Daftar"   
-                                            onClick={() => window.location.href = selectedDivisi.link}
-                                            className="text-white max-w-[180px]"
+                            <div className="flex flex-col md:flex-row items-center text-center">
+                                <div className="flex-row mx-4 w-full mb-4">
+                                    <h2 className="text-2xl font-bold">{selectedDivisi.name}</h2>
+                                    <h3 className="text-lg font-semibold">{selectedDivisi.nama}</h3>
+                                    <div className="flex justify-center my-4">
+                                        <img
+                                            src={selectedDivisi.logoImg}
+                                            alt={selectedDivisi.name}
+                                            className="w-[120px] object-contain rounded-full shadow-md"
                                         />
                                     </div>
+                                    <div className="bg-gray-200 p-2 rounded-lg border w-full">
+                                        <p className="text-sm">Logo: {selectedDivisi.logo}</p>
+                                        <p className="text-sm">Arti: {selectedDivisi.arti}</p>
+                                    </div>
+                                </div>
+                                <div className="">
+                                    <div className="foto-divisi">
+                                        <ImageSlider images={images} interval={3000} />
+                                    </div>
+                                    <div className="bg-gray-200 p-4 mt-4 rounded-lg border w-full mb-[20px]">
+                                        <p className="text-sm text-justify">{selectedDivisi.deskripsi}</p>
+                                        <p className="text-sm mt-2 text-justify">{selectedDivisi.tugas}</p>
+                                    </div>
+                                    <GlassButton
+                                        text="Daftar"   
+                                        onClick={() => window.location.href = selectedDivisi.link}
+                                        className="text-white max-w-[180px]"
+                                    />
                                 </div>
                             </div>
                         </motion.div>
                     </div>
-                </AnimatePresence>
-            )}
+                )}
+            </AnimatePresence>
         </>
     );
 }
