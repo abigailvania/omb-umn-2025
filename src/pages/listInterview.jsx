81821a2c02bd0e7259e77a3de4da3726994b11bb
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-13 18:46:50 +0700

    rapihin

--- src/pages/listInterview.jsx
+++ src/pages/listInterview.jsx
@@ -6,78 +6,47 @@ const listInterview = [
         logoDivisi: logo1,
         jadwal: {
             senin: [
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
             ],
             selasa: [
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
             ],
             rabu: [
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
             ],
             kamis: [
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
             ],
             jumat: [
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
             ],
             sabtu: [
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
-                {
-                    nama: " ",
-                    nim: " ",
-                    jam: " "
-                },
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
+                {nama: " ", nim: " ", jam: " "},
             ],
-            
 
         }
     },
