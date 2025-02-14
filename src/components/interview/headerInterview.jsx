ccf9050003ae3a6efe3ae6ec452ce6d0b7a143a7
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 21:21:49 +0700

    tes

--- src/components/interview/headerInterview.jsx
+++ src/components/interview/headerInterview.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function headerInterview() {
+function HeaderInterview() {
     return (
         <h1>LIST DIVISI</h1>
     )
 }
-export default headerInterview;
+export default HeaderInterview;
