34c6b256deab0531eacb8acb277d344645945b05
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 12:21:40 +0700

    mencoba benerin routing (saya lupa cara bikinnya)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Landing from "./pages/landing";
+import Interview from "./pages/interview";
+import HalamanError from "./pages/halamanerror";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Landing/>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/interview" element={<Interview />} />
+        <Route path="*" element={<HalamanError />} />
+      </Routes>
+    </Router>
   );
 }
 
