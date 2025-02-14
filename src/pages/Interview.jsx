6891a109de37c52bd7a2966367abf40db36e6845
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 12:39:13 +0700

    navbar2

--- src/pages/Interview.jsx
+++ src/pages/Interview.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/navbar/Navbar";
+import Navbar from "../components/navbar/navbar2";
 // import ApaItuOMB from "../components/apa-itu-omb/ApaItuOMB";
 
 import Footer from "../components/footer/Footer";
