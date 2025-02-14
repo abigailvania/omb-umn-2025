3bdd5d11a12bedf3333fa14ae610c54b02a4bd45
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 21:44:22 +0700

    header interview

--- src/components/interview/headerInterview.jsx
+++ src/components/interview/headerInterview.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function HeaderInterview() {
     return (
-        <h1>LIST DIVISI</h1>
-    )
+        <div className="mt-[50px] text-center">
+            <h1 className="text-2xl font-bold">Daftar Peserta Interview</h1>
+            <p className="text-lg">Mohon Memperhatikan Tanggal dan Waktu interview</p>
+        </div>
+    );
 }
+
 export default HeaderInterview;
