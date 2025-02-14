6a43448f57e5ac515bff74c17828a0587409d6e0
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 21:44:57 +0700

    halaman interview

--- src/pages/Interview.jsx
+++ src/pages/Interview.jsx
@@ -4,6 +4,7 @@ import HeaderInterview from "../components/interview/headerInterview";
 import Peraturan from "../components/interview/aturanInterview";
 import NavbarDivisi from "../components/interview/navbarDivisi";
 
+import Footer from "../components/footer/Footer";
 
 const Interview = () => {
   return (
@@ -12,6 +13,7 @@ const Interview = () => {
       <Navbar />
       <HeaderInterview />
       <NavbarDivisi />
+      <Footer />
     </>
   );
 };
