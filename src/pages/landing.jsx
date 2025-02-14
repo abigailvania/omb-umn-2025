dbb6807fdd98a3a0971ae000fc708eef4efa3105
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 11:58:15 +0700

    revisi landing

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -6,7 +6,7 @@ import Youtube from "../components/youtube/Youtube";
 import Oprec from "../components/oprec/Oprec";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
-import Interview from "./pages/Interview";
+import Interview from "./Interview";
 
 function App() {
   return (
