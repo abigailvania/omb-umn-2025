b9bd817ee7b7e85c28b98b2ef64ed1857338e2c7
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 11:52:37 +0700

    menambahkan link untuk navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
                         </li>
                         <li>
                         <a
-                            href="#"
+                            href="/interview"
                             className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
                         >
                             Daftar Interview
