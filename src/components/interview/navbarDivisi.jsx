90c256c6efbe402e4adb4d3a728cabb699bcc11d
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 21:07:38 +0700

    test divisi

--- src/components/interview/navbarDivisi.jsx
+++ src/components/interview/navbarDivisi.jsx
@@ -1,22 +1,41 @@
 import React, { useState } from "react";
-import listInterview from "./daftarInterview";
+import listInterview from "./listInterview";
 
 function NavbarDivisi() {
     const [selectedDivisi, setSelectedDivisi] = useState(null);
 
+    console.log("Total Divisi:", listInterview.length);
+
     return (
-        <div className="z-900">
+        <div>
             <div>
+                <p>Total Divisi: {listInterview.length}</p>
                 {listInterview.map((divisi, index) => (
                     <button
                         key={index}
+                        onClick={() => setSelectedDivisi(divisi)}
                     >
                         {divisi.divisi}
                     </button>
                 ))}
             </div>
             
-            
+            {selectedDivisi && (
+                <div>
+                    <h2>{selectedDivisi.divisi}</h2>
+                    <img src={selectedDivisi.logoDivisi} alt={selectedDivisi.divisi} />
+                    <div>
+                        <h3>Jadwal Hari 1:</h3>
+                        <ul>
+                            {selectedDivisi.jadwal.hari1.map((item, idx) => (
+                                <li key={idx}>
+                                    {item.nama} - {item.nim} - {item.jam}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
