86d942e27284bd748532e75251a85bcfe95c0fa1
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 11:56:43 +0700

    revisi landing

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from "./components/header/Header";
-import ApaItuOMB from "./components/apa-itu-omb/ApaItuOMB";
-import Youtube from "./components/youtube/Youtube";
-import Oprec from "./components/oprec/Oprec";
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/navbar/Navbar";
+import Header from "./src/components/header/Header";
+import ApaItuOMB from "./src/components/apa-itu-omb/ApaItuOMB";
+import Youtube from "./src/components/youtube/Youtube";
+import Oprec from "./src/components/oprec/Oprec";
+import Footer from "./src/components/footer/Footer";
+import Navbar from "./src/components/navbar/Navbar";
 import Interview from "./pages/Interview";
 
 function App() {
