5e5b2e52bf69278961d060b808599511bc33cef1
Abigail <abigailvaniapra@gmail.com>
2025-02-14 19:48:39 +0700

    Landing

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/header/Header.jsx";
+import ApaItuOMB from "../components/apa-itu-omb/ApaItuOMB.jsx";
+import Youtube from "../components/youtube/Youtube.jsx";
+import Oprec from "../components/oprec/Oprec.jsx";
+import Footer from "../components/footer/Footer.jsx";
+
+const Landing = () => {
+    return (
+        <>
+            <Header />
+            <ApaItuOMB />
+            <Youtube />
+            <Oprec />
+            <Footer />
+        </>
+    );
+};
+
+export default Landing;
