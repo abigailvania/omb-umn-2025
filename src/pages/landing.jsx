cae967320b15510e58953211b59069a592f46d15
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-14 12:35:46 +0700

    .

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,24 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import React from "react";
 import Header from "../components/header/Header";
 import ApaItuOMB from "../components/apa-itu-omb/ApaItuOMB";
 import Youtube from "../components/youtube/Youtube";
 import Oprec from "../components/oprec/Oprec";
 import Footer from "../components/footer/Footer";
-import Navbar from "../components/navbar/Navbar";
-import Interview from "./Interview";
 
-function App() {
+const Landing = () => {
   return (
-    <Router>
+    <>
       <Header />
-      <Navbar />
-            <ApaItuOMB />
-            <Youtube />
-            <Oprec />
+      <ApaItuOMB />
+      <Youtube />
+      <Oprec />
       <Footer />
-    </Router>
+    </>
   );
-}
+};
 
-export default App;
+export default Landing;
