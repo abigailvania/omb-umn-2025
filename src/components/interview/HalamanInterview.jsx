a29f256a35adfb71beb749d8d95d58d85eae9d9b
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-16 00:50:46 +0700

    tes

--- src/components/interview/HalamanInterview.jsx
+++ src/components/interview/HalamanInterview.jsx
@@ -107,7 +107,7 @@ const InterviewPage = () => {
               </div>
             </div>
           )}
-          <div className="ombak mb-[-90px]">
+          <div className="ombak mb-[-150px]">
             <img src={ombak} alt="ombak" className="w-full relative block p-0" />
         </div>
       </div>
