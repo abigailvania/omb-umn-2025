5f54df129e71e6b577441da66830da180bd52de5
Abigail <abigailvaniapra@gmail.com>
2025-02-16 00:21:31 +0700

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "./Footer.scss";
-
 import logo from "../../images/header-footer/hitam_glowterang1080.png";
 import tagline from "../../images/header-footer/tagline-01.png";
 import email from "../../images/header-footer/email.png";
@@ -10,38 +8,34 @@ import line from "../../images/header-footer/line.png";
 
 function Footer() {
     return (
-        <div className="footerSection relative w-full bg-[url('/foto-footer-1.png')]">
-            <div className="content">
-                <div className="flex flex-row justify-between items-center mt-50 flexfooter">
-                    <div className="flex-1 text-center order-2
-                                    md:text-right md:order-1">
+        <div className="relative w-full bg-[url('/foto-footer-1.png')] bg-cover">
+            <div className="relative z-2 p-3 -mt-[5px] p-2 sm: lg:p-5 text-white text-center">
+                <div className="flex flex-col md:flex-row justify-between items-center mt-5">
+                    <div className="flex-1 text-center md:text-right mb-5 md:mb-0">
                         <p className="text-xl font-bold">Alamat</p>
                         <p>Universitas Multimedia Nusantara</p>
                         <p>Jl. Scientia Boulevard, Gading Serpong,</p>
                         <p>Tangerang Banten 15811, Indonesia.</p>
                     </div>
-                    <div className="flex-1 flex flex-col items-center order-first flexfooter
-                                    -mt-[100px] md:mt-0 md:order-2">
+                    <div className="flex-1 flex flex-col items-center mb-5 md:mb-0">
                         <img src={logo} alt="Logo OMB UMN 2025" className="h-[200px] md:h-[270px] object-contain" />
-                        <img src={tagline} alt="Tagline OMB UMN 2025" className="h-[70px] -mt-[20px] object-contain" />
+                        <img src={tagline} alt="Tagline OMB UMN 2025" className="h-[70px] mt-[-20px] object-contain" />
                     </div>
-                    <div className="flex-1 text-center md:text-left order-3">
+                    <div className="flex-1 text-center md:text-left">
                         <p className="text-xl font-bold">Pranala</p>
                         <p>YouTube: @ombumn</p>
                         <p>Spotify: Swara Bestari</p>
                         <p>Website: omb.umn.ac.id</p>
                     </div>
                 </div>
-                <div className="contact flex flex-col justify-between items-center mt-4 mb-[50px]">
-                    <div className="contact-icon flex flex-row flex-wrap md:flex-nowrap justify-center gap-x-4">
-                        <a href=""> <img src={email} alt="email" className="w-[30px] mx-[10px]" /></a>
-                        <a href="https://www.instagram.com/ombumn"> <img src={insta} alt="instagram" className="w-[25px] mx-[10px]" /></a>
-                        <a href="https://linevoom.line.me/user/_dQto-n6wJSbyUlsQVlEfNHoStsNJwzSLtfVj2Xo"> <img src={line} alt="line" className="w-[25px] mx-[10px]" /></a>
-                        <a href="https://www.facebook.com/ombumn/?_rdc=2&_rdr"> <img src={facebook} alt="facebook" className="w-[25px] mx-[10px]" /></a>
+                <div className="flex flex-col items-center mt-4 mb-5">
+                    <div className="flex flex-row justify-center gap-4 mb-5">
+                        <a href=""><img src={email} alt="email" className="w-7" /></a>
+                        <a href="https://www.instagram.com/ombumn"><img src={insta} alt="instagram" className="w-6" /></a>
+                        <a href="https://linevoom.line.me/user/_dQto-n6wJSbyUlsQVlEfNHoStsNJwzSLtfVj2Xo"><img src={line} alt="line" className="w-6" /></a>
+                        <a href="https://www.facebook.com/ombumn/?_rdc=2&_rdr"><img src={facebook} alt="facebook" className="w-6" /></a>
                     </div>
-                    <div className="mt-12 md:my-6 mb-[50px]">
-                        <p>© OMB UMN 2025</p>
-                    </div>
+                    <p className="text-sm">© OMB UMN 2025</p>
                 </div>
             </div>
         </div>
