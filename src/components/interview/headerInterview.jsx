5ba06edc41beebc91b612e576b8bbb235814ea20
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-15 19:41:24 +0700

    DIKITTTTT

--- src/components/interview/headerInterview.jsx
+++ src/components/interview/headerInterview.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function HeaderInterview() {
     return (
-        <div className="mt-[120px] text-center">
+        <div className="mt-[125px] text-center">
             <h1 className="text-2xl font-bold">Daftar Peserta Interview</h1>
             <p className="text-lg">Mohon Memperhatikan Tanggal dan Waktu interview</p>
         </div>
