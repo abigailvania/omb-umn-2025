25fe9faff011afa56b2d46b6818e7cb636ca33e7
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-15 19:29:21 +0700

    tes bg halaman interview

--- src/pages/Interview.jsx
+++ src/pages/Interview.jsx
@@ -3,18 +3,25 @@ import Navbar from "../components/navbar/Navbar2";
 import HeaderInterview from "../components/interview/headerInterview";
 import Peraturan from "../components/interview/aturanInterview";
 import NavbarDivisi from "../components/interview/navbarDivisi";
-
 import Footer from "../components/footer/Footer";
 
 const Interview = () => {
+  const backgroundStyle = {
+    backgroundImage: "url('./images/gradien/gradien2.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    minHeight: "100vh", 
+    width: "100%",
+  };
+
   return (
-    <>
+    <div style={backgroundStyle}>
       <Peraturan />
       <Navbar />
       <HeaderInterview />
       <NavbarDivisi />
       <Footer />
-    </>
+    </div>
   );
 };
 
