c5ae2a5e69cd119b4872dc6481c4d54fa4551d5e
TipenHans <167948766+TipenHans@users.noreply.github.com>
2025-02-16 10:14:33 +0700

    coba ini

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
 
     return (
         <>
-            <section id="home-section" className="headerSection ">
+            <section id="home-section" className="headerSection max-w-full">
                 <div className="headerContent">
                     <div className="leftColumn">
                         <div className="supergrafis absolute z-0 opacity-40">
